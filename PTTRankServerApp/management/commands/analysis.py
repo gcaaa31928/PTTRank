a16ead3abc4ab8b9d2e0286fbddc665d6c0ee715
gcaaa31928 <gcaaa31928@gmail.com>
2016-07-27 04:14:48 +0800

    added top comments analysis

--- PTTRankServerApp/management/commands/analysis.py
+++ PTTRankServerApp/management/commands/analysis.py
@@ -1,3 +1,6 @@
+# -*- coding: utf-8 -*-
+import operator
+
 from django.core import serializers
 from django.core.management import BaseCommand
 from PTTRankServerApp.models import *
@@ -8,7 +11,22 @@ class Command(BaseCommand):
     help_text = 'my test command'
 
     def handle(self, *args, **options):
-        ptt = PTT.objects.get(pk=1)
-        serializer = PTTSerializer(ptt)
-        print(serializer.data['comments'][0]['user'])
-        print(serializer.data)
+       self.top_comments()
+
+    def top_comments(self):
+        ptt = PTT.objects.all()
+        ptt_json = PTTSerializer(ptt, many=True).data
+        user_comments_times = dict()
+        for json in ptt_json:
+            comments = json['comments']
+            for comment in comments:
+                user = comment['user']
+                if user in user_comments_times:
+                    user_comments_times[user] += 1
+                else:
+                    user_comments_times[user] = 1
+
+        sorted_result = sorted(user_comments_times.items(), key=operator.itemgetter(1), reverse=True)
+        for k, v in sorted_result:
+            print(k,v)
+
