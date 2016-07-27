020bbc1939f7e42fd77af9e534e8fa07c10adcd5
gcaaa31928 <gcaaa31928@gmail.com>
2016-07-28 04:23:39 +0800

    added post freq

--- PTTRankServerApp/management/commands/analysis.py
+++ PTTRankServerApp/management/commands/analysis.py
@@ -16,27 +16,8 @@ class Command(BaseCommand):
     def handle(self, *args, **options):
         rank_object = Rank.objects.create()
         rank_object.top_comments = PTTHelper.top_comments(datetime.datetime(2017, 1, 3))
+        high_quality_post, low_quality_post = PTTHelper.post_quality(datetime.datetime(2005, 1, 1))
+        rank_object.high_quality_post = high_quality_post
+        rank_object.low_quality_post = low_quality_post
+        PTTHelper.post_frequency(datetime.datetime(2005, 1, 1))
         rank_object.save()
-
-
-    def post_quality(self, rank_object):
-        ptt = PTT.objects.all()
-        ptt_json = PTTSerializer(ptt, many=True).data
-        author_score = dict()
-        for article in ptt_json:
-            author_id = article['author_id']
-            score = article['score']
-            if author_id in author_score:
-                author_score[article['author_id']] += score
-            else:
-                author_score[article['author_id']] = score
-
-        descending_result = sorted(author_score.items(), key=operator.itemgetter(1), reverse=True)
-        high_quality = dict()
-        low_quality = dict()
-        for k, v in descending_result[:20]:
-            high_quality[k] = v
-        for k, v in descending_result[-20:]:
-            low_quality[k] = v
-        rank_object.high_quality_post = high_quality
-        rank_object.low_quality_post = low_quality
--- PTTRankServerApp/ptt_helper.py
+++ PTTRankServerApp/ptt_helper.py
@@ -5,9 +5,8 @@ from PTTRankServerApp.serializers import PTTSerializer
 
 
 class PTTHelper:
-
     @classmethod
-    def top_comments(cls, after_datetimes, limit = 20):
+    def top_comments(cls, after_datetimes, limit=20):
 
         ptt = PTT.objects.filter(date__gt=after_datetimes)
         ptt_json = PTTSerializer(ptt, many=True).data
@@ -25,12 +24,11 @@ class PTTHelper:
         top_comments = dict()
         for k, v in sorted_result[:limit]:
             top_comments[k] = v
-        print(top_comments)
         return top_comments
 
     @classmethod
-    def post_quality(cls):
-        ptt = PTT.objects.all()
+    def post_quality(cls, after_datetimes, limit=20):
+        ptt = PTT.objects.filter(date__gt=after_datetimes)
         ptt_json = PTTSerializer(ptt, many=True).data
         author_score = dict()
         for article in ptt_json:
@@ -44,8 +42,30 @@ class PTTHelper:
         descending_result = sorted(author_score.items(), key=operator.itemgetter(1), reverse=True)
         high_quality = dict()
         low_quality = dict()
-        for k, v in descending_result[:20]:
+        for k, v in descending_result[:limit]:
             high_quality[k] = v
-        for k, v in descending_result[-20:]:
+        for k, v in descending_result[-limit:]:
             low_quality[k] = v
         return high_quality, low_quality
+
+    @classmethod
+    def post_frequency(cls, after_datetimes, limit=20):
+        ptt = PTT.objects.filter(date__gt=after_datetimes)
+        ptt_json = PTTSerializer(ptt, many=True).data
+        author_freq = dict()
+        for article in ptt_json:
+            author_id = article['author_id']
+            if author_id in author_freq:
+                author_freq[author_id] += 1
+            else:
+                author_freq[author_id] = 1
+
+        descending_result = sorted(author_freq.items(), key=operator.itemgetter(1), reverse=True)
+        high_freq = dict()
+        low_freq = dict()
+        for k, v in descending_result[:limit]:
+            high_freq[k] = v
+        for k, v in descending_result[-limit:]:
+            low_freq[k] = v
+        print(high_freq, low_freq)
+        return high_freq, low_freq
