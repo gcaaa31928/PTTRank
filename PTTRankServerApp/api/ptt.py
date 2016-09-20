edae3a63a9fc185990b25d82548a417874506e8e
gcaaa31928 <gcaaa31928@gmail.com>
2016-09-20 16:44:35 +0800

    fixed timezone issues from hot topic api

--- PTTRankServerApp/api/ptt.py
+++ PTTRankServerApp/api/ptt.py
@@ -1,21 +1,30 @@
 from datetime import datetime
+
+from dateutil.tz import tzlocal
+from django.utils.timezone import activate
+from pytz import timezone
 import time
 
 from rest_framework import status
 from rest_framework.decorators import api_view
 
+from PTTRankServer import settings
 from PTTRankServerApp.views import JSONResponse
 from ..ptt_helper import *
 from ..serializers import *
 
+activate(settings.TIME_ZONE)
+
 def epoch_to_datetime(current_epoch):
-    current_datetime = datetime.utcfromtimestamp(float(current_epoch))
+    current_datetime = datetime.fromtimestamp(float(current_epoch))
+    # current_datetime = datetime.utcfromtimestamp(float(current_epoch))
     return current_datetime
 
 @api_view(['GET'])
 def hot_topic(request):
     start_date = epoch_to_datetime(request.GET.get('start_epoch', None))
     end_date = epoch_to_datetime(request.GET.get('end_epoch', int(time.mktime(datetime.now().timetuple()))))
+    print(start_date)
     limit = int(request.GET.get('limit', 20))
     start_page = int(request.GET.get('start_page', 0))
     articles = PTTHelper.hot_topic(start_date, end_date, start_page * limit, limit, threshold=10)
